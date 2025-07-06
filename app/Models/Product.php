46b220b57982666385d424633bfa35c2b01bb41d
Shym2501 <sulthannurya@gmail.com>
2025-07-06 19:43:45 +0800

    Add Log Act Feature

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,9 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Spatie\Activitylog\Traits\LogsActivity;
+use Spatie\Activitylog\LogOptions;
 
 class Product extends Model
 {
+    use HasFactory, LogsActivity;
+
     /**
      * The attributes that are mass assignable.
      *
@@ -15,6 +20,17 @@ class Product extends Model
         'name',
         'description',
     ];
+
+    // Fungsi untuk kustomisasi log
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+            ->logOnly(['name', 'email']) // Kolom yang ingin dilacak
+            ->logOnlyDirty() // <-- PENTING: Hanya catat jika ada perubahan
+            ->dontSubmitEmptyLogs() // Jangan buat log jika tidak ada yang berubah
+            ->setDescriptionForEvent(fn(string $eventName) => "Customer '{$this->name}' has been {$eventName}")
+            ->useLogName('Customer');
+    }
     public function licenseKeys()
     {
         return $this->hasMany(LicenseKey::class);
